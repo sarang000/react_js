278f0a71b02f714271304cce559f70deac4b784a
sarang000 <4mh19ec082@gmail.com>
2023-04-05 12:27:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [countdown, setCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  const targetDate = new Date('2023-12-31T23:59:59');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = targetDate.getTime() - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setCountdown({ days, hours, minutes, seconds });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>New Year's Eve Countdown</h1>
+      <div className="countdown">
+        <div className="countdown-item">
+          <span className="countdown-value">{countdown.days}</span>
+          <span className="countdown-label">Days</span>
+        </div>
+        <div className="countdown-item">
+          <span className="countdown-value">{countdown.hours}</span>
+          <span className="countdown-label">Hours</span>
+        </div>
+        <div className="countdown-item">
+          <span className="countdown-value">{countdown.minutes}</span>
+          <span className="countdown-label">Minutes</span>
+        </div>
+        <div className="countdown-item">
+          <span className="countdown-value">{countdown.seconds}</span>
+          <span className="countdown-label">Seconds</span>
+        </div>
+      </div>
     </div>
   );
 }
